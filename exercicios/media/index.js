de0a1be8a8a829ec673ffa23816015a8e0fd9a50
unknown <vinicius45anacleto@gmail.com>
2022-04-28 20:25:54 -0300

    commit

--- exercicios/media/index.js
+++ exercicios/media/index.js
@@ -2,16 +2,11 @@ var contador = 0
 var res = 0
 
 function adicionar(){
-    Number(res)
-    console.log(res)
     var num1 = Number(document.getElementById('valor').value)
-    document.getElementById('valor').value = ''
+    document.getElementById('valor').value = '' 
     res = num1 + res
-    console.log(res)
     contador++
-    console.log(contador)
     media = res / contador
-    console.log(media) 
-    var res = document.getElementById('res')
-    res.innerHTML = (`A Média das notas é ${media}`)
+    var div = document.getElementById('res')
+    div.innerHTML = (`A Média das notas é ${media}`)
 }
